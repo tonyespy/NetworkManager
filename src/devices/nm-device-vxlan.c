7179be9e4702ade32bb46507121c7b404fd7e04d
Dan Williams <dcbw@redhat.com>
2014-09-24 15:13:19 -0500

    core: add class function for device unrealization
    When a device is unrealized, its class-specific properties must also be cleared
    since the device is no longer valid.

--- src/devices/nm-device-vxlan.c
+++ src/devices/nm-device-vxlan.c
@@ -22,9 +22,9 @@
 
 #include <string.h>
 
+#include "nm-default.h"
 #include "nm-device-vxlan.h"
 #include "nm-device-private.h"
-#include "nm-default.h"
 #include "nm-manager.h"
 #include "nm-platform.h"
 #include "nm-utils.h"
@@ -141,6 +141,27 @@ setup (NMDevice *device, NMPlatformLink *plink)
 	update_properties (device);
 }
 
+static void
+unrealize (NMDevice *device, gboolean remove_resources)
+{
+	NMDeviceVxlan *self = NM_DEVICE_VXLAN (device);
+	NMDeviceVxlanPrivate *priv = NM_DEVICE_VXLAN_GET_PRIVATE (self);
+	GParamSpec **properties;
+	guint n_properties, i;
+
+	g_object_freeze_notify (G_OBJECT (device));
+
+	NM_DEVICE_CLASS (nm_device_vxlan_parent_class)->unrealize (device, remove_resources);
+
+	memset (&priv->props, 0, sizeof (NMPlatformVxlanProperties));
+
+	properties = g_object_class_list_properties (G_OBJECT_GET_CLASS (self), &n_properties);
+	for (i = 0; i < n_properties; i++)
+		g_object_notify_by_pspec (G_OBJECT (self), properties[i]);
+	g_free (properties);
+
+	g_object_thaw_notify (G_OBJECT (device));
+}
 
 /**************************************************************/
 
@@ -230,6 +251,7 @@ nm_device_vxlan_class_init (NMDeviceVxlanClass *klass)
 
 	device_class->link_changed = link_changed;
 	device_class->setup = setup;
+	device_class->unrealize = unrealize;
 
 	/* properties */
 	g_object_class_install_property
