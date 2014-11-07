6aafe201795d20335d5cf7a697c81501ce969dfd
Dan Williams <dcbw@redhat.com>
2014-11-07 08:51:28 -0600

    fixup! dhcp: move D-Bus DHCP listener into separate class

--- src/dhcp-manager/nm-dhcp-listener.c
+++ src/dhcp-manager/nm-dhcp-listener.c
@@ -13,8 +13,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Copyright (C) 2005 - 2013 Red Hat, Inc.
- * Copyright (C) 2006 - 2008 Novell, Inc.
+ * Copyright 2014 Red Hat, Inc.
  *
  */
 
