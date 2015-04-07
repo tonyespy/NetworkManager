tonyespy/NetworkManager

commit 600489003ff2eca33865a3d41abcc9fc03fd6bb1

device: set the reason for when deactivating for another activation
It's always user requested -- auto activation never happens on already active
devices. nm_device_release_one_slave() rightly asserts teardown with
(un)configuration does not happen for no reason at all.

https://bugzilla.gnome.org/show_bug.cgi?id=744812

src/devices/nm-device.c
@@ -5834,7 +5834,7 @@ nm_device_queue_activation (NMDevice *self, NMActRequest *req)
 		_LOGI (LOGD_DEVICE, "disconnecting for new activation request.");
 		nm_device_state_changed (self,
 		                         NM_DEVICE_STATE_DEACTIVATING,
-		                         NM_DEVICE_STATE_REASON_NONE);
+		                         NM_DEVICE_STATE_REASON_USER_REQUESTED);
 	}
 }
 

commit 61b1900e1080406be6a1bbf177ebc90e97a62e8f

device: use "new-activation" reason when deactivating for another activation
"user-requested" has a side effect of disabling autoconnect.

Fixes: 600489003ff2eca33865a3d41abcc9fc03fd6bb1

src/devices/nm-device.c
@@ -5816,7 +5816,7 @@ nm_device_queue_activation (NMDevice *self, NMActRequest *req)
 		_LOGI (LOGD_DEVICE, "disconnecting for new activation request.");
 		nm_device_state_changed (self,
 		                         NM_DEVICE_STATE_DEACTIVATING,
-		                         NM_DEVICE_STATE_REASON_USER_REQUESTED);
+		                         NM_DEVICE_STATE_REASON_NEW_ACTIVATION);
 	}
 }
 