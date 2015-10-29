600cab1da82cac1e1434f4beff7c608b167f3d6c
Thomas Haller <thaller@redhat.com>
2015-10-29 15:12:32 +0100

    core: add nm_utils_monotonic_timestamp_as_boottime() function

--- src/NetworkManagerUtils.c
+++ src/NetworkManagerUtils.c
@@ -2169,12 +2169,12 @@ nm_utils_cmp_connection_by_autoconnect_priority (NMConnection **a, NMConnection 
 /**************************************************************************/
 
 static gint64 monotonic_timestamp_offset_sec;
+static gboolean monotonic_timestamp_initialized = FALSE;
 
 static void
 monotonic_timestamp_get (struct timespec *tp)
 {
 	static int clock_mode = 0;
-	gboolean first_time = FALSE;
 	int err = 0;
 
 	switch (clock_mode) {
@@ -2186,7 +2186,6 @@ monotonic_timestamp_get (struct timespec *tp)
 			err = clock_gettime (CLOCK_MONOTONIC, tp);
 		} else
 			clock_mode = 1;
-		first_time = TRUE;
 		break;
 	case 1:
 		/* default, return CLOCK_BOOTTIME */
@@ -2202,7 +2201,7 @@ monotonic_timestamp_get (struct timespec *tp)
 	g_assert (err == 0); (void)err;
 	g_assert (tp->tv_nsec >= 0 && tp->tv_nsec < NM_UTILS_NS_PER_SECOND);
 
-	if (G_LIKELY (!first_time))
+	if (G_LIKELY (monotonic_timestamp_initialized))
 		return;
 
 	/* Calculate an offset for the time stamp.
@@ -2219,6 +2218,7 @@ monotonic_timestamp_get (struct timespec *tp)
 	 * wraps (~68 years).
 	 **/
 	monotonic_timestamp_offset_sec = (- ((gint64) tp->tv_sec)) + 1;
+	monotonic_timestamp_initialized = TRUE;
 
 	if (nm_logging_enabled (LOGL_DEBUG, LOGD_CORE)) {
 		time_t now = time (NULL);
@@ -2231,6 +2231,51 @@ monotonic_timestamp_get (struct timespec *tp)
 		                       tp->tv_nsec, (long long) -monotonic_timestamp_offset_sec,
 		                       clock_mode == 1 ? "CLOCK_BOOTTIME" : "CLOCK_MONOTONIC", s);
 	}
+}
+
+/**
+ * nm_utils_monotonic_timestamp_as_boottime:
+ * @timestamp: the monotonic-timestamp that should be converted into CLOCK_BOOTTIME.
+ * @timestamp_ns_per_tick: How many nano seconds make one unit of @timestamp? E.g. if
+ * @timestamp is in unit seconds, pass %NM_UTILS_NS_PER_SECOND; @timestamp in nano
+ * seconds, pass 1; @timestamp in milli seconds, pass %NM_UTILS_NS_PER_SECOND/1000; etc.
+ *
+ * Returns: the monotonic-timestamp as CLOCK_BOOTTIME, as returned by clock_gettime().
+ * The unit is the same as the passed in @timestamp basd on @timestamp_ns_per_tick.
+ * E.g. if you passed @timestamp in as seconds, it will return boottime in seconds.
+ * If @timestamp is a non-positive, it returns -1. Note that a (valid) monotonic-timestamp
+ * is always positive.
+ **/
+gint64
+nm_utils_monotonic_timestamp_as_boottime (gint64 timestamp, gint64 timestamp_ns_per_tick)
+{
+	gint64 offset;
+
+	/* only support ns-per-tick being a multiple of 10. */
+	g_return_val_if_fail (timestamp_ns_per_tick == 1
+	                      || (timestamp_ns_per_tick > 0 &&
+	                          timestamp_ns_per_tick <= NM_UTILS_NS_PER_SECOND &&
+	                          timestamp_ns_per_tick % 10 == 0),
+	                      -1);
+
+	/* Check that the timestamp is in a valid range. */
+	g_return_val_if_fail (timestamp >= 0, -1);
+
+	/* timestamp == 0 is not a valid timestamp. Treat it special and return -1. */
+	if (timestamp <= 0)
+		return -1;
+
+	/* if the caller didn't yet ever fetch a monotonic-timestamp, he cannot pass any meaningful
+	 * value (because he has no idea what these timestamps would be). That would be a bug. */
+	g_return_val_if_fail (G_LIKELY (monotonic_timestamp_initialized), -1);
+
+	/* calculate the offset of monotonic-timestamp to boottime. offset_s is <= 1. */
+	offset = monotonic_timestamp_offset_sec * (NM_UTILS_NS_PER_SECOND / timestamp_ns_per_tick);
+
+	/* check for overflow. */
+	g_return_val_if_fail (offset>0 || timestamp < G_MAXINT64 + offset, G_MAXINT64);
+
+	return timestamp - offset;
 }
 
 /**
