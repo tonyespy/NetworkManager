ca5622360faa66205fb3324f4e3dc34c9ef38135
Beniamino Galvani <bgalvani@redhat.com>
2016-02-05 08:47:38 +0100

    libnm-core: make ipvx.dhcp-timeout signed
    Change the dhcp-timeout property in NMSettingIPConfig to int type for
    consistency with the dad-timeout property. For dad-timeout -1 means
    "use default value", while for dhcp-timeout probably we will never use
    negative values, but it seems more correct to use the same type for
    the two properties.

--- clients/cli/settings.c
+++ clients/cli/settings.c
@@ -6477,7 +6477,7 @@ nmc_properties_init (void)
 	                    NULL);
 	nmc_add_prop_funcs (GLUE_IP (4, DHCP_TIMEOUT),
 	                    nmc_property_ipv4_get_dhcp_timeout,
-	                    nmc_property_set_uint,
+	                    nmc_property_set_int,
 	                    NULL,
 	                    NULL,
 	                    NULL,
@@ -6626,7 +6626,7 @@ nmc_properties_init (void)
 	                    NULL);
 	nmc_add_prop_funcs (GLUE_IP (6, DHCP_TIMEOUT),
 	                    nmc_property_ipv6_get_dhcp_timeout,
-	                    nmc_property_set_uint,
+	                    nmc_property_set_int,
 	                    NULL,
 	                    NULL,
 	                    NULL,
